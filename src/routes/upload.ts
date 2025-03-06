fb478e7f0ddb2b24eaf1a1a0e6287a6ea2e373ac
Armando <armando.scuotto.studenti@isii.it>
2025-03-06 20:50:18 +0100

    Added route for image load

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -23,28 +23,28 @@ router.post("/", upload.single("file"), async (request: Request, response: Respo
     }
 
     const requiredParams = [
-        "id", "type"
+      "id", "type"
     ];
-    
+
     const { sanitizedParams, missingParams } = sanitizeParams(requiredParams, request.query);
-    
+
     if (missingParams.length > 0) {
-        return response.status(422).send(response.__("missingRequiredParamsError") + missingParams.map((p => response.__(p))).join(", "));
+      return response.status(422).send(response.__("missingRequiredParamsError") + missingParams.map((p => response.__(p))).join(", "));
     }
 
-    let vehicle = await prisma.vehicle.findFirst({ where: { id: Number(sanitizedParams.id), idUser:  (request.user as User).id} });
-    if ( !!vehicle && !!vehicle[sanitizedParams.type as keyof Vehicle] && vehicle[sanitizedParams.type as keyof Vehicle] != path.extname(request.file.originalname)){
+    let vehicle = await prisma.vehicle.findFirst({ where: { id: Number(sanitizedParams.id), idUser: (request.user as User).id } });
+    if (!!vehicle && !!vehicle[sanitizedParams.type as keyof Vehicle] && vehicle[sanitizedParams.type as keyof Vehicle] != path.extname(request.file.originalname)) {
       let fileName = sanitizedParams.id + "-" + sanitizedParams.type + vehicle[sanitizedParams.type as keyof Vehicle];
       const filePath = path.join(__dirname, "..", "..", "uploads", fileName);
       fs.unlinkSync(filePath);
     }
 
     await prisma.vehicle.update({
-      where: { 
+      where: {
         id: Number(sanitizedParams.id),
         idUser: (request.user as User).id,
       },
-      data: { 
+      data: {
         [sanitizedParams.type as keyof Vehicle]: path.extname(request.file.originalname)
       },
     });
@@ -61,14 +61,14 @@ router.get("/", async (request: Request, response: Response): Promise<any> => {
       return response.status(401).send(response.__("unauthorizedError"));
     }
     const requiredParams = [
-        "id", "type"
+      "id", "type"
     ];
     const { sanitizedParams, missingParams } = sanitizeParams(requiredParams, request.query);
     if (missingParams.length > 0) {
-        return response.status(422).send(response.__("missingRequiredParamsError") + missingParams.map((p => response.__(p))).join(", "));
+      return response.status(422).send(response.__("missingRequiredParamsError") + missingParams.map((p => response.__(p))).join(", "));
     }
-    let vehicle = await prisma.vehicle.findFirst({ where: { id: Number(sanitizedParams.id), idUser:  (request.user as User).id} });
-    if (!vehicle){
+    let vehicle = await prisma.vehicle.findFirst({ where: { id: Number(sanitizedParams.id), idUser: (request.user as User).id } });
+    if (!vehicle) {
       return response.status(404).json(response.__("vehicleNotFoundError"));
     }
     if (!vehicle[sanitizedParams.type as keyof Vehicle]) {
@@ -87,7 +87,7 @@ router.get("/", async (request: Request, response: Response): Promise<any> => {
   }
 })
 
-router.get("/all", async (request: Request, response: Response): Promise<any> => {
+router.get("/image", async (request: Request, response: Response): Promise<any> => {
   try {
     if (!request.isAuthenticated()) {
       return response.status(401).send(response.__("unauthorizedError"));
@@ -95,21 +95,19 @@ router.get("/all", async (request: Request, response: Response): Promise<any> =>
     const requiredParams = ["id"];
     const { sanitizedParams, missingParams } = sanitizeParams(requiredParams, request.query);
     if (missingParams.length > 0) {
-        return response.status(422).send(response.__("missingRequiredParamsError") + missingParams.map((p => response.__(p))).join(", "));
+      return response.status(422).send(response.__("missingRequiredParamsError") + missingParams.map((p => response.__(p))).join(", "));
     }
-    let vehicle = await prisma.vehicle.findFirst({ where: { id: Number(sanitizedParams.id), idUser:  (request.user as User).id} });
-    if (!vehicle){
+    let vehicle = await prisma.vehicle.findFirst({ where: { id: Number(sanitizedParams.id), idUser: (request.user as User).id } });
+    if (!vehicle) {
       return response.status(404).json(response.__("vehicleNotFoundError"));
     }
     let files: any = {
-      registrationCardFileExtension: false,
-      maintenanceFileExtension: false,
-      insuranceFileExtension: false
+      vehicleImageFileExtension: false
     };
 
     Object.keys(files).forEach((type) => {
       let fileName = sanitizedParams.id + "-" + type + vehicle[type as keyof Vehicle];
-      
+
       const filePath = path.join(__dirname, "..", "..", "uploads", fileName);
       if (fs.existsSync(filePath)) {
         files[type] = true;
@@ -122,34 +120,69 @@ router.get("/all", async (request: Request, response: Response): Promise<any> =>
   }
 })
 
+  router.get("/all", async (request: Request, response: Response): Promise<any> => {
+    try {
+      if (!request.isAuthenticated()) {
+        return response.status(401).send(response.__("unauthorizedError"));
+      }
+      const requiredParams = ["id"];
+      const { sanitizedParams, missingParams } = sanitizeParams(requiredParams, request.query);
+      if (missingParams.length > 0) {
+        return response.status(422).send(response.__("missingRequiredParamsError") + missingParams.map((p => response.__(p))).join(", "));
+      }
+      let vehicle = await prisma.vehicle.findFirst({ where: { id: Number(sanitizedParams.id), idUser: (request.user as User).id } });
+      if (!vehicle) {
+        return response.status(404).json(response.__("vehicleNotFoundError"));
+      }
+      let files: any = {
+        registrationCardFileExtension: false,
+        maintenanceFileExtension: false,
+        insuranceFileExtension: false
+      };
+  
+      Object.keys(files).forEach((type) => {
+        let fileName = sanitizedParams.id + "-" + type + vehicle[type as keyof Vehicle];
+  
+        const filePath = path.join(__dirname, "..", "..", "uploads", fileName);
+        if (fs.existsSync(filePath)) {
+          files[type] = true;
+        }
+      })
+      response.json(files);
+    } catch (error) {
+      response.status(500).send(response.__("serverError"));
+      console.error(error);
+    }
+  })
+
 router.delete("/", async (request: Request, response: Response): Promise<any> => {
   try {
     if (!request.isAuthenticated()) {
       return response.status(401).send(response.__("unauthorizedError"));
     }
     const requiredParams = [
-        "id", "type"
+      "id", "type"
     ];
 
     const { sanitizedParams, missingParams } = sanitizeParams(requiredParams, request.query);
-    
+
     if (missingParams.length > 0) {
-        return response.status(422).send(response.__("missingRequiredParamsError") + missingParams.map((p => response.__(p))).join(", "));
+      return response.status(422).send(response.__("missingRequiredParamsError") + missingParams.map((p => response.__(p))).join(", "));
     }
 
-    let vehicle = await prisma.vehicle.findFirst({ where: { id: Number(sanitizedParams.id), idUser:  (request.user as User).id} });
-    if ( !!vehicle && !!vehicle[sanitizedParams.type as keyof Vehicle]){
+    let vehicle = await prisma.vehicle.findFirst({ where: { id: Number(sanitizedParams.id), idUser: (request.user as User).id } });
+    if (!!vehicle && !!vehicle[sanitizedParams.type as keyof Vehicle]) {
       let fileName = sanitizedParams.id + "-" + sanitizedParams.type + vehicle[sanitizedParams.type as keyof Vehicle];
       const filePath = path.join(__dirname, "..", "..", "uploads", fileName);
       fs.unlinkSync(filePath);
     }
 
     await prisma.vehicle.update({
-      where: { 
+      where: {
         id: Number(sanitizedParams.id),
         idUser: (request.user as User).id,
       },
-      data: { 
+      data: {
         [sanitizedParams.type as keyof Vehicle]: null
       },
     });
