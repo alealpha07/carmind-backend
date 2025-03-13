241d44b9827ea89bd7a99b1330d1d07130b530e0
Alessandro Prati <alealphatv@gmail.com>
2025-03-13 10:55:47 +0100

    changed subscribe from if approach to upsert

--- src/routes/subscribe.ts
+++ src/routes/subscribe.ts
@@ -16,10 +16,12 @@ router.post("/", isAuthenticated, async (request: Request, response: Response): 
         
         const user = (request.user as User);
         const {endpoint,keys} = sanitizedParams;
-        const existing = await prisma.subscription.findUnique({ where: { endpoint } });
-        if (!existing) {
-            await prisma.subscription.create({ data: { endpoint, p256dh: keys.p256dh, auth: keys.auth, idUser: user.id }});
-        }
+        //update if it exists => create if it doesn't
+        await prisma.subscription.upsert({
+            where: { endpoint },
+            update: { p256dh: keys.p256dh, auth: keys.auth, idUser: user.id },
+            create: { endpoint, p256dh: keys.p256dh, auth: keys.auth, idUser: user.id },
+        });
         response.status(201).json({ message: response.__("subscribedSuccesfully") });
     } catch (error) {
         response.status(500).send(response.__("serverError"));
